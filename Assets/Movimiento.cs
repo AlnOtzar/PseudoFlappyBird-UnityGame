beec091472ac410f1fc94923af589013b0072092
MauAlcantara1 <japonxd1@hotmail.com>
2025-02-06 19:53:00 -0600

    Version 3 con caida de pajaro y fondo

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    [SerializeField] private float upForce = 350f;
+
+    private bool estaMuerto;
+    private Rigidbody2D playerRB;
+    private Animator playerAnimator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>(); // Inicialización del Animator
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !estaMuerto)
+        {
+            Flap();
+        }
+    }
+
+    private void Flap()
+    {
+        playerRB.velocity = Vector2.zero;
+        playerRB.AddForce(Vector2.up * upForce);
+        playerAnimator.SetTrigger("Flap");
+    }
+        
+    private void OnCollisionEnter2D()
+    {
+        estaMuerto = true;
+        playerAnimator.SetTrigger("Die");
+    }
+}
