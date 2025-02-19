1aeef2af9b833c984033d8859acf32e353f56ec1
AlnOtzar”git config --global user.email [200~aotzar2003@gmail.com” <aotzar2003@gmail.com>
2025-02-19 08:56:51 -0600

    botones hola

---
+++ Assets/Scripts/Info.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Info : MonoBehaviour
+{
+    public void Volver(string VolverMenu)
+    {
+        SceneManager.LoadScene(Informacion);
+    }
+
+
+}
