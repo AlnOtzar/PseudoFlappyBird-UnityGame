7be9576465964cf90ed5d0768c1b548eb4a6a52e
AlnOtzar”git config --global user.email [200~aotzar2003@gmail.com” <aotzar2003@gmail.com>
2025-03-03 18:02:55 -0600

    Sonidos Integrados v1

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Audio : MonoBehaviour
+{
+    private AudioSource music; // Nombre correcto de la variable
+    public AudioClip ClickAudio;
+    public AudioClip switchAudio;
+
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+    }
+
+    public void ClickAudioOn()
+    {
+        music.PlayOneShot(ClickAudio);
+    }
+
+    public void SwitchAudioOn()
+    {
+        music.PlayOneShot(switchAudio);
+    }
+}
